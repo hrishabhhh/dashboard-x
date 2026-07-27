25d3ce4f027a50589531e9db31b483d449a97ed6
Hrishabh Jain <hrishabhsinghai77@gmail.com>
2026-07-27 17:56:51 +0530

    Implemented theme using custom useTheme hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-// import { useState } from "react";
-
 import "./App.css";
 import AppRouter from "./routes/AppRouter";
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
   return (
     <>
-      <section id="center">
+      <section
+        id="center"
+        className={
+          theme === "dark"
+            ? "bg-gray-900 text-white min-h-screen"
+            : "bg-white text-black min-h-screen"
+        }
+      >
         <AppRouter />
       </section>
     </>
--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -1,13 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext(null);
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
 
   function toggleTheme() {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
   }
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
