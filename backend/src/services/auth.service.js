a61fd10c901bbf598bedd402444ea4ed636f5ed8
Hrishabh Jain <hrishabhsinghai77@gmail.com>
2026-07-30 23:36:53 +0530

    Implement registration service with bcrypt setup

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,1 +1,22 @@
-export async function registerUser(userData) {}
+import userAccount from "../models/account.model";
+import bcrypt from "bcrypt";
+export async function registerUser(userData) {
+  //destructure data
+  const { name, email, password } = userData;
+  const SALT_ROUNDS = 10;
+  const isVerified = false;
+  const emailResponse = await userAccount.findOne({
+    email,
+  });
+  if (emailResponse) {
+    throw new AppError("User already exists", 400);
+  } else {
+    const hashed_password = await bcrypt.hash(password, SALT_ROUNDS);
+    const newUser = await userAccount.create({
+      name,
+      email,
+      isVerified: true,
+    });
+    return newUser;
+  }
+}
