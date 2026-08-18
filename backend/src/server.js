58e3f7b37882fb5e3cd342562ea241990a4dcaa8
Hrishabh Jain <hrishabhsinghai77@gmail.com>
2026-08-19 01:59:25 +0530

    Fallback added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 
 // dotenv.config();
 import { connectDB } from "./config/db.js";
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 await connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
