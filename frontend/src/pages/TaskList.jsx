b33e74fc49d7d75b09bf800c168dae0a9531e18f
Hrishabh Jain <hrishabhsinghai77@gmail.com>
2026-08-26 17:25:03 +0530

    add task edit permissions on frontend

--- frontend/src/pages/TaskList.jsx
+++ frontend/src/pages/TaskList.jsx
@@ -3,16 +3,25 @@ import { useState } from "react";
 import { getTasks } from "../api/tasks";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
+import { getUsers } from "../api/users";
+import { useAuth } from "../hooks/useAuth";
 
 function TaskList() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-
+  const [currentRegisteredUser, setCurrentRegisteredUser] = useState(null);
+  const { user } = useAuth();
   useEffect(() => {
     async function loadTasks() {
       try {
         const data = await getTasks();
+        const usersData = await getUsers();
+        const matchedUsers = usersData.find(
+          (registeredUser) =>
+            String(registeredUser.account) === String(user?._id),
+        );
+        setCurrentRegisteredUser(matchedUsers || null);
         setTasks(data.tasks);
       } catch (error) {
         setError(error.message);
@@ -31,39 +40,49 @@ function TaskList() {
   }
   return (
     <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
-      {tasks.map((task) => (
-        <div
-          key={task._id}
-          className="rounded-2xl border border-gray-300 bg-white p-5 text-black shadow"
-        >
-          <h3 className="mb-2 text-xl font-bold">{task.title}</h3>
+      {tasks.map((task) => {
+        const canEdit =
+          currentRegisteredUser &&
+          String(task.assignedTo?._id) === String(currentRegisteredUser?._id);
+        return (
+          <div
+            key={task._id}
+            className="rounded-2xl border border-gray-300 bg-white p-5 text-black shadow"
+          >
+            <h3 className="mb-2 text-xl font-bold">{task.title}</h3>
 
-          <p className="mb-4">{task.description}</p>
+            <p className="mb-4">{task.description}</p>
 
-          <p>
-            <strong>Assigned To:</strong> {task.assignedTo?.name || "N/A"}
-          </p>
+            <p>
+              <strong>Assigned To:</strong> {task.assignedTo?.name || "N/A"}
+            </p>
 
-          <p>
-            <strong>Assigned By:</strong> {task.assignedBy?.name || "N/A"}
-          </p>
+            <p>
+              <strong>Assigned By:</strong> {task.assignedBy?.name || "N/A"}
+            </p>
 
-          <p>
-            <strong>Status:</strong> {task.status || "N/A"}
-          </p>
+            <p>
+              <strong>Status:</strong> {task.status || "N/A"}
+            </p>
 
-          <p>
-            <strong>Priority:</strong> {task.priority || "N/A"}
-          </p>
+            <p>
+              <strong>Priority:</strong> {task.priority || "N/A"}
+            </p>
 
-          <p>
-            <strong>Due Date:</strong>{" "}
-            {task.dueDate
-              ? new Date(task.dueDate).toLocaleDateString("en-IN")
-              : "N/A"}
-          </p>
-        </div>
-      ))}
+            <p>
+              <strong>Due Date:</strong>{" "}
+              {task.dueDate
+                ? new Date(task.dueDate).toLocaleDateString("en-IN")
+                : "N/A"}
+            </p>
+            {canEdit && (
+              <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-colors duration-300 mt-2">
+                Edit Task
+              </button>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
