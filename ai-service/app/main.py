66c6d743f5bb390d1fe99d1cc2c5d103551aa122
Hrishabh Jain <hrishabhsinghai77@gmail.com>
2026-09-09 23:29:28 +0530

    add task schema validation for AI service

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.schemas.task import TaskInput
 
 app = FastAPI()
 
@@ -7,4 +8,11 @@ def root():
     return {
         "service": "Dashboard-X AI Service",
         "status": "running"
+    }
+
+@app.post("/risk/test")
+def root(task : TaskInput): 
+    return {
+        "recieved": True,
+        "task": task
     }
---
+++ ai-service/app/schemas/task.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pydantic import BaseModel
+from enum import Enum
+
+class TaskStatus(str,Enum):
+    pending = "pending"
+    in_progress = "in_progress"
+    completed = "completed"
+
+class TaskPriority(str,Enum):
+    low = "low"
+    medium = "medium"
+    hign = "high"
+
+class TaskInput(BaseModel):
+    id: str
+    title: str
+    description: str | None = None
+    status: TaskStatus
+    priority: TaskPriority
+    dueDate: datetime
+    assignedTo: str
+    assignedBy: str
+
+
+
